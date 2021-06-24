fe0ecd48ddd075eb692cecb2648d22080f18077d
geneaudrey <geneaudreytan99@gmail.com>
2021-06-25 01:06:41 +0800

    added express.static('dist') ..?

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ db.connect((err) => {
 
 const app = express();
 
-// app.use(express.static("dist"));
+app.use(express.static("dist"));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
