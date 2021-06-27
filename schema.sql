ef3ac53ab12efcfdc046b06fafd62b74e24aa5f0
geneaudrey <geneaudreytan99@gmail.com>
2021-06-27 21:25:11 +0800

    edited schema.sql

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@ CREATE TABLE users( id INT AUTO_INCREMENT, username VARCHAR(255), password VARCH
 CREATE TABLE alarms( id INT AUTO_INCREMENT, userID INT, time VARCHAR(255), isActive BOOL, repetitions VARCHAR(255), ampm VARCHAR(255), PRIMARY KEY (id), FOREIGN KEY (userID) REFERENCES users(id)
 ON DELETE CASCADE );
 
-CREATE TABLE settings(id int AUTO_INCREMENT, userID int, difficulty int, mode int, PRIMARY KEY (id), FOREIGN KEY (userID) REFERENCES users(id)
+CREATE TABLE settings(id int AUTO_INCREMENT,  userID int, difficulty VARCHAR(255), mode int, PRIMARY KEY (id), FOREIGN KEY (userID) REFERENCES users(id) 
 ON DELETE CASCADE);
 
-CREATE TABLE statistics(id int AUTO_INCREMENT, userID int, snoozes int, timeToWake VARCHAR(255), wakeUpTime VARCHAR(255), sleepQuality int, dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id), FOREIGN KEY (userID) REFERENCES users(id)
+CREATE TABLE statistics(id int AUTO_INCREMENT,  userID int, snoozes int, timeToWake VARCHAR(255), wakeUpTime VARCHAR(255), mood int, dateCreated DATETIME DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id), FOREIGN KEY (userID) REFERENCES users(id) 
 ON DELETE CASCADE);
