ed418d618f34c28c42b74ce00b194f4df7b7d50a
limlance99 <limlance_99@yahoo.com>
2021-06-27 14:35:26 +0800

    attempting to fix cors issue

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(express.static("dist"));
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://cs176smartalarm.herokuapp.com/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Credentials', true)
     next();
